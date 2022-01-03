88cd6efc022d167c9bef2810d8252514923c83b2
Lucy Woodman <lucyjwoodman@gmail.com>
2022-01-03 15:35:19 +0000

    feat: setup connection to MongoDB and test

--- run.py
+++ run.py
@@ -1,5 +1,22 @@
+# Third party imports
 import os
-if os.path.exists('settings.py'):
-    from settings import username, password
+from pymongo import MongoClient
 
-print(username, password)
+# Local application imports
+if os.path.exists('settings.py'):
+    from settings import username, password, mongodb_string
+
+client = MongoClient(mongodb_string)
+
+db = client.gettingStarted
+people = db.people
+
+personDocument = {
+    "name": {"first": "Alan", "last": "Turing"},
+    "contribs": ["Turing machine", "Turing test", "Turingery"],
+    "views": 1250000
+}
+
+people.insert_one(personDocument)
+person = people.find_one({"name.last": "Turing"})
+print(person)
