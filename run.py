4247d85f3951578a18287e56b654a2cac7676aa5
Lucy Woodman <lucyjwoodman@gmail.com>
2022-01-04 17:41:34 +0000

    feat: create function to allow users to register

--- run.py
+++ run.py
@@ -1,22 +1,52 @@
 # Third party imports
 import os
+import getpass
+import hashlib
+from xkcdpass import xkcd_password as xp
 from pymongo import MongoClient
 
 # Local application imports
 if os.path.exists('settings.py'):
-    from settings import username, password, mongodb_string
+    from settings import mongodb_string
 
+# Connect to MongoDB and set the database variables
 client = MongoClient(mongodb_string)
+db = client.supportStats
+users = db.users
+stats = db.stats
 
-db = client.gettingStarted
-people = db.people
 
-personDocument = {
-    "name": {"first": "Alan", "last": "Turing"},
-    "contribs": ["Turing machine", "Turing test", "Turingery"],
-    "views": 1250000
-}
+def create_password():
+    """
+    * Creates a randomly generated password using the xkcdpass library.
+    * @return(str) gen_password -- the password.
+    """
+    word_file = xp.locate_wordfile()
+    words = xp.generate_wordlist(
+        wordfile=word_file, min_length=5, max_length=10)
 
-people.insert_one(personDocument)
-person = people.find_one({"name.last": "Turing"})
-print(person)
+    gen_password = xp.generate_xkcdpassword(words, numwords=3, delimiter="-")
+    return gen_password
+
+
+def register():
+    """
+    * Allows new users to register using a chosen username and
+    * generated password from create_password().
+    * Hashes the password and saves to the connected database.
+    """
+    username = input('Enter a username: ')
+    pwd = create_password()
+    print('Please save the password somewhere safe.')
+    print(f'Your password is: {pwd}')
+
+    enc_pwd = pwd.encode()
+    hash_pwd = hashlib.md5(enc_pwd).hexdigest()
+
+    newUser = {
+        "username": username,
+        "password": hash_pwd
+    }
+
+    db.users.insert_one(newUser)
+    print('You have successfully registered!')
