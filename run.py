0baf2d512cd30b98510181ba4fb4f89e9a2a0d25
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-12 15:47:49 +0100

    refac: move main menu into the main function and simplify

--- run.py
+++ run.py
@@ -1,40 +1,53 @@
-# Third party imports
-import pyfiglet
+import os
 
 # Local application imports
-from menus import Menu, Main, Sub
+from welcome import welcome
+from login import login, register
 
 
-def welcome():
-    """
-    * Welcome banner for the program.
-    * Displays ascii robot and title.
-    """
-    ascii_banner = pyfiglet.figlet_format("Stats Bot!!")
-    bot = r"""
-         ___T_
-        | O O |
-        |__u__|
-      (m9\:::/\
-         /___\6
-          |_|
-         (ooo)
-    """
-    print('=' * 80)
-    print('WELCOME TO:')
-    print(ascii_banner + bot)
+# def welcome():
+#     """
+#     * Welcome banner for the program.
+#     * Displays ascii robot and title.
+#     """
+#     ascii_banner = pyfiglet.figlet_format("Stats Bot!!")
+#     bot = r"""
+#          ___T_
+#         | O O |
+#         |__u__|
+#       (m9\:::/\
+#          /___\6
+#           |_|
+#          (ooo)
+#     """
+#     print('=' * 80)
+#     print('WELCOME TO:')
+#     print(ascii_banner + bot)
 
 
 def main():
     """
     * Main program function where it all begins.
     """
-    welcome()
-    Menu.generate_menu(Main)
-    loggedin = Menu.run(Main)
-    while loggedin:
-        Menu.generate_menu(Sub)
-        Menu.run(Sub)
+    while True:
+        os.system('clear')
+        print(welcome('Stats bot'))
+        print('\nChoose an option : ')
+        print('''
+            1 : LOGIN
+            2 : REGISTER
+            0 : EXIT'''
+              )
+        choice = input('\nEnter your choice : ')
+
+        if choice == '1':
+            login()
+        elif choice == '2':
+            register()
+        elif choice == '0':
+            print('\nStay awesome!\n')
+            exit()
+        os.system('clear')
 
 
 if __name__ == '__main__':
