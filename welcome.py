16849b1506e8ef4ce07f0acd6c9138e8393cfa16
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-12 16:08:44 +0100

    refac: move the ascii bot to the welcome file to keep run.py clean

--- welcome.py
+++ welcome.py
@@ -3,6 +3,24 @@ from colorama import Fore, Back, Style
 from pyfiglet import Figlet
 
 
-def welcome(text):
+def welcome(string):
+    """
+    PyFiglet and coloured string conversion
+    """
     result = Figlet()
-    return colored.cyan(result.renderText(text))
+    return colored.cyan(result.renderText(string))
+
+
+def stats_bot():
+    """
+    * Displays an ascii robot.
+    """
+    bot = r'''               ___T_
+              | O O |
+              |__u__|
+            (m9\:::/\
+               /___\6
+                |_|
+               (ooo)
+    '''
+    return bot
