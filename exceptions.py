00e0431879d26bcef6e95f1ea1e733242166ddc8
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-13 17:34:14 +0100

    feat: add a custom exception for when the password doesn't match the db

--- exceptions.py
+++ exceptions.py
@@ -1,9 +1,23 @@
+from multiprocessing import AuthenticationError
+
+
 class UsernameError(LookupError):
     """ Raised when the username is incorrect """
 
     def __init__(self, username):
         self.username = username
         self.message = f'\n** Username: "{self.username}" cannot be found in the database. **\n'
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class PasswordError(AuthenticationError):
+    """ Raised when the user password doesn't match the stored password """
+
+    def __init__(self):
+        self.message = f'\n** The password is incorrect. **\n'
         super().__init__(self.message)
 
     def __str__(self):
