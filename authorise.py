60dfadd05e436c02d99a20ed2dddbbd4ff6d8bf1
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-14 11:37:25 +0100

    docs: add further code comments to explain the functions

--- authorise.py
+++ authorise.py
@@ -17,18 +17,23 @@ def login():
     current_user = User()
     while tries > 0:
         test_database()
+        # Ask for a username
         current_user.username = input('Username : ')
         try:
+            # Check the database for the username
             user_count = db.users.count_documents(
                 {"_User__username": current_user.username})
             if user_count == 0:
                 raise LookupError(
                     f'\n** Username: "{current_user.username}" cannot be found in the database. **\n')
 
+            # Ask for a password
             current_user.password = input('\nPassword : ')
             try:
+                # Fetch the user's details from the database
                 db_user = db.users.find_one(
                     {"_User__username": current_user.username})
+                # Check if supplied password matches the password in the database
                 if db_user["_hashed_password"] != current_user._hashed_password:
                     raise ValueError(f'\n** The password is incorrect. **\n')
 
