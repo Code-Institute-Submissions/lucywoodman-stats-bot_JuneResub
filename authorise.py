dea8ced29504881355146070b46967bbdf3baecc
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-14 11:34:38 +0100

    docs: add further code comments to explain the functions

--- authorise.py
+++ authorise.py
@@ -63,25 +63,30 @@ def register():
         test_database()
         new_user = User()
         try:
+            # Ask for a username
             new_user.username = input('Enter a username : ')
+            # Check if the username is blank/empty
             if not new_user.username:
                 raise ValueError(f'\n** Username cannot be blank. **\n')
         except ValueError as e:
             print(e)
         else:
             try:
+                # Check the database for the username
                 user_count = db.users.count_documents(
                     {"_User__username": new_user.username})
                 if user_count:
                     raise ValueError(
                         f'\n** The username "{new_user.username}" is already registered. **\n')
 
+                # Ask for a password and save the user to the database
                 new_user.password = input('Enter a password : ')
                 db.users.insert_one(new_user.__dict__)
                 print(
                     f'\n>> Great! "{new_user.username}" has been registered. You can now login.')
                 print('>> Taking you back to the main menu...')
                 time.sleep(3)
+                # Return to the main menu (run.py)
                 break
 
             except ValueError as e:
