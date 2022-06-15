912a7e57e49814a0cddab8a8c86fc8d1ad4b0793
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-15 17:52:54 +0100

    feat: pass username to main app to allow for personalised welcome message

--- statbotic/authorise.py
+++ statbotic/authorise.py
@@ -43,7 +43,7 @@ def login():
                 print('Taking you to the statistics menu...')
                 time.sleep(2)
                 # Go to main app
-                app()
+                app(current_user)
 
             except ValueError as e:
                 print(e)
