de249be3a35a7c20310c8d3638f22d01ce917b71
Lucy Woodman <lucyjwoodman@gmail.com>
2022-06-15 16:22:45 +0100

    chore: resave the helper after all other functions removed

--- helper.py
+++ helper.py
@@ -1,43 +1,5 @@
-# Third party imports
-import os
-from pymongo import MongoClient, errors
 from tabulate import tabulate
-import certifi
-
-# Local application imports
-if os.path.exists('env.py'):
-    import env  # pylint: disable=unused-import
-
-MONGODB_URI = os.environ.get('MONGODB_URI')
-
-# Connect to MongoDB and set the database variables
-client = MongoClient(host=MONGODB_URI,
-                     tlsCAFile=certifi.where(), serverSelectionTimeoutMS=5000)
-db = client.supportStats
-users = db.users
-stats = db.stats
-
-
-class Title():
-    def __init__(self, title):
-        self.title = title
-
-    def display(self):
-        print('-' * len(self.title))
-        print(self.title)
-        print('-' * len(self.title))
-
-
-def test_database():
-    """
-    * Tests the db connection
-    * Raises an exception and exits if there are issues.
-    """
-    try:
-        users.find_one()
-    except errors.ConnectionFailure:
-        print('The database says no. Try again later.')
-        exit()
+from title import Title
 
 
 def user_continue(question):
@@ -52,8 +14,8 @@ def user_continue(question):
             user_input.lower()
             if user_input not in ('y', 'n'):
                 raise ValueError('The input did not match "y" or "n".')
-        except ValueError:
-            print('The input did not match "y" or "n".')
+        except ValueError as e:
+            print(e)
         else:
             return True if user_input == 'y' else False
 
@@ -91,84 +53,3 @@ def print_stats(title, data):
 
     # Print the list as a table.
     print(tabulate(table_list, tablefmt="fancy_grid", numalign="decimal"))
-
-
-def fetch_data_range(start, end):
-    data = db.stats.aggregate([
-        {
-            # Fetch the data between the starting and ending dates.
-            '$match': {
-                'date': {
-                    '$gte': start,
-                    '$lte': end
-                }
-            }
-        }
-    ])
-    return data
-
-
-def aggregate_data(start, end):
-    data = db.stats.aggregate([
-        {
-            # Fetch the data between the starting and ending dates.
-            '$match': {
-                'date': {
-                    '$gte': start,
-                    '$lte': end
-                }
-            }
-        }, {
-            # Group the data and calculate the column totals.
-            '$group': {
-                '_id': 'null',
-                'Total tickets advanced': {
-                    '$sum': '$advanced'
-                },
-                'Total ticket public comments': {
-                    '$sum': '$comments'
-                },
-                'Total tickets solved': {
-                    '$sum': '$solved'
-                },
-                'Total chats': {
-                    '$sum': '$total'
-                },
-                'Average ticket public comments per day': {
-                    '$avg': '$comments'
-                },
-                'Average tickets solved per day': {
-                    '$avg': '$solved'
-                },
-                'Average chats per day': {
-                    '$avg': '$total'
-                },
-                'Average chat wait time': {
-                    '$avg': '$wait'
-                },
-                'Average chat CSAT for the week': {
-                    '$avg': '$csat'
-                }
-            }
-        }, {
-            # Add comments per solve ratio.
-            '$project': {
-                'Total tickets advanced': 1,
-                'Total ticket public comments': 1,
-                'Total tickets solved': 1,
-                'Total chats': 1,
-                'Average ticket public comments per day': 1,
-                'Average tickets solved per day': 1,
-                'Average chats per day': 1,
-                'Average chat wait time': 1,
-                'Average chat CSAT for the week': 1,
-                'Average public comments per ticket solved': {
-                    '$divide': [
-                        '$Average ticket public comments per day',
-                        '$Average tickets solved per day'
-                    ]
-                }
-            }
-        }
-    ])
-    return data
